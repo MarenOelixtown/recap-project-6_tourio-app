d46c80ee74bd45e137c4256866d8fda192f8647c
Maren <marenehlers@gmx.net>
2024-06-20 12:35:10 +0200

    replacing POST-request-methode from api-id-index to api-index

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -21,16 +21,4 @@ export default async function handler(request, response) {
 
     response.status(200).json(place);
   }
-  if (request.method === "POST") {
-    console.log("POST REQUEST!");
-    try {
-      const placeData = request.body;
-      console.log("placeData: ", placeData);
-      await Places.create(placeData);
-      response.status(201).json({ status: "Place created." });
-    } catch (error) {
-      console.log(error);
-      response.status(400).json({ error: error.message });
-    }
-  }
 }
--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -8,6 +8,19 @@ export default async function handler(request, response) {
   if (request.method === "GET") {
     const places = await Places.find();
     return response.status(200).json(places);
+  }
+
+  if (request.method === "POST") {
+    console.log("POST REQUEST!");
+    try {
+      const placeData = request.body;
+      console.log("placeData: ", placeData);
+      await Places.create(placeData);
+      response.status(201).json({ status: "Place created." });
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error: error.message });
+    }
   } else {
     return response.status(405).json({ message: "Method not allowed" });
   }
