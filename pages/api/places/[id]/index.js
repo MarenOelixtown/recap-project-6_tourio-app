358a387a77c9478d3e77f23dab04972cefa56610
Maren <marenehlers@gmx.net>
2024-06-20 11:55:59 +0200

    adding places-model in api-id-index

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,17 +1,24 @@
-import { places } from "../../../../lib/db.js";
+/* import { places } from "../../../../lib/db.js"; */
+import dbConnect from "../../../../db/connect.js";
+import Places from "../../../../db/models/Places.js";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
+
   const { id } = request.query;
 
   if (!id) {
     return;
   }
 
-  const place = places.find((place) => place.id === id);
+  if (request.method === "GET") {
+    /* const place = places.find((place) => place.id === id); */
+    const place = await Places.findById(id);
 
-  if (!place) {
-    return response.status(404).json({ status: "Not found" });
+    if (!place) {
+      return response.status(404).json({ status: "Not found" });
+    }
+
+    response.status(200).json(place);
   }
-
-  response.status(200).json(place);
 }
